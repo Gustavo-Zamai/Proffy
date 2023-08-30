0422c9338f368221f47f3b5a0769b012489ef093
Gustavo Simão Zamai <gustavosimaozamai@gmail.com>
2023-08-30 10:54:05 -0300

    Create API and finish routes of classes and connections

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,12 @@
-import express, { response as res, request as req } from "express";
+import express from "express";
+import cors from 'cors';
+import routes from "./routes";
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 
-
-app.get("/", (req, res) => {
-    return res.json({ messsage: 'Ashen One' });
-});
+app.use(routes);
 
 app.listen(3333);
